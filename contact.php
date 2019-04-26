280bdd8df0db39b9e907b9f0b91bdf610759178e
ansellyuen <36493070+ansellyuen@users.noreply.github.com>
2019-04-26 00:21:26 -0500

    Update contact.html and contact.php

--- contact.php
+++ contact.php
@@ -19,7 +19,36 @@ if(empty($sender)||empty($senderEmail)||empty($message))
 
     mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
 
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
+    echo "Submission Received";
 }
 
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+               
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
 ?>
