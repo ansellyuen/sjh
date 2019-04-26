28a278ead049a88457fae0ee10793e3dc286217a
ansellyuen <36493070+ansellyuen@users.noreply.github.com>
2019-04-26 00:02:54 -0500

    Contact Update
    Linked contact.html with contact.php

--- contact.php
+++ contact.php
@@ -1,11 +1,19 @@
 <?php
 
-if($_POST["submit"]) {
+if((!isset($_POST['submit'])) {
+    echo 'Error; You need to submit the form.'
+}
     $recipient="ansellyuen@gmail.com";
-    $subject="Form to email message";
+    $subject="Contact Us Message";
     $sender=$_POST["sender"];
     $senderEmail=$_POST["senderEmail"];
     $message=$_POST["message"];
+//validate first
+if(empty($sender)||empty($senderEmail)||empty($message))
+   {
+       echo "Fill out required fields";
+       exit;
+   }
 
     $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
 
